9dcd2b29100b22b7f5deaa000e11748f76f63bc4
DESKTOP-IPJVOSK\sing <zptmxj@naver.com>
2022-05-05 18:45:33 +0900

    220505

--- src/page/component/Member/Member.js
+++ src/page/component/Member/Member.js
@@ -11,31 +11,31 @@ function Member(props)
         <div className="list">
             <div className="list-line">
                 <p className="list-black">{props.idx}</p>
-                <div className="list-img">
+                <div >
                 {
                     props.data.privilege===2?
-                    <img src={sub_mg} width='25px' height='25px'/>:
+                    <img className="list-img" src={sub_mg} />:
                     (props.data.privilege===3?
-                    <img src={super_mg} width='25px' height='25px'/>:
+                    <img className="list-img" src={super_mg} />:
                     (props.data.privilege===4?
-                    <img src={eng_mg} width='25px' height='25px'/>:
+                    <img className="list-img" src={eng_mg} />:
                     null
                     ))
                 }
                 </div>
-                <p> / </p>
+                <p className="list-black"> / </p>
                 <p className="list-black">{ props.data.name}</p>
-                <p> / </p>
+                <p className="list-black"> / </p>
                 <div className="list-black">
                 {
                     props.data.sex.data[0]?
-                    <img src={img_girl} width='30px' height='30px'/>:
-                    <img src={img_boy} width='30px' height='30px'/>
+                    <img className="list-img" src={img_girl}/>:
+                    <img className="list-img" src={img_boy}/>
                 }
                 </div>
-                <p> / </p>
+                <p className="list-black"> / </p>
                 <p className="list-black">{ (props.Year+ 1) - (1900+props.data.age)}</p>
-                <p> / </p>
+                <p className="list-black"> / </p>
                 <p className="list-black">{ props.data.adddate.substr(0,10)}</p>
 
             </div>

6221de8cc36de5801fb35c9bc031bed8bbf37cd0
DESKTOP-IPJVOSK\sing <zptmxj@naver.com>
2023-01-22 21:54:38 +0900

    230122

--- src/page/Notice.js
+++ src/page/Notice.js
@@ -1,5 +1,5 @@
 import './Notice.scss';
-import {Form,Col,Row,Table } from 'react-bootstrap';
+import {Accordion } from 'react-bootstrap';
 import {serverPath,imagePath} from '../IP_PORT';
 import { useEffect, useState } from 'react';
 import {useDispatch, useSelector} from 'react-redux'
@@ -15,18 +15,16 @@ function Notice(props)
     const Year = curDate.getFullYear();
     
     const [sel, setSel] = useState(-1);
-    const [noticeList, setNoticeList] = useState();
+    const [noticeList, setNoticeList] = useState([]);
     const [isButton,setIsButton] = useState(false);
     const [allStatus, setAllStatus] = useState([]);
     const [scrollY, setScrollY] = useState(0);
 
 
     useEffect(()=>{
-
-
-        if(allStatus.length==0)
+        if(noticeList.length==0)
         {
-            fetch(serverPath()+"/out_allstatus",{
+            fetch(serverPath()+"/out_notice",{
                 method:"post",
                 headers : {
                     "content-type" : "application/json",
@@ -36,59 +34,32 @@ function Notice(props)
             .then((res)=>res.json())
             .then((all)=>{
                 console.log('all',all);
-                let memlists = member.map((e,i)=>{
-                    let memlist = {targetuid:e.uid,rea:0,role:0,stra:0,oper:0,agil:0,gamb:0};
-          
-                    for(let j = 0 ; j<all.length;j++)
-                    {  
-                        if(member[i].uid==all[j].targetuid)
-                        {
-                            memlist.rea += all[j].rea;
-                            memlist.role += all[j].role;
-                            memlist.stra += all[j].stra;
-                            memlist.oper += all[j].oper;
-                            memlist.agil += all[j].agil;
-                            memlist.gamb += all[j].gamb;
-                        }
-                    }
-                    return memlist;
-                })
-                console.log('memlists_sel',memlists,sel);
-                setAllStatus(memlists);
+            
+                setNoticeList(all);
 
             });
         }
-        if(sel==-1)
-        {
-            window.scrollTo({top:scrollY,behavior:"instant"});
-            //window.scrollTo(0,scrollY);
-            console.log('window.scrollTo',scrollY);
-        }
-        else
-        {
-            window.scrollTo({top:400,behavior:"instant"});
-        }
     })
 
-    const setBackCallback = ()=>{
-        setIsButton(false);
-        setSel(-1);
-    }
+    // const setBackCallback = ()=>{
+    //     setIsButton(false);
+    //     setSel(-1);
+    // }
 
-    const onSend = ()=>{
-        console.log('onSend');
-        if(!isButton) setIsButton(true);
-        else
-        {
-            setIsButton(false);
-            setAllStatus([]);
-        }
-    }
+    // const onSend = ()=>{
+    //     console.log('onSend');
+    //     if(!isButton) setIsButton(true);
+    //     else
+    //     {
+    //         setIsButton(false);
+    //         setAllStatus([]);
+    //     }
+    // }
 
-    const onCancel = ()=>{
-        console.log('onCancel');
-        setIsButton(false)
-    }
+    // const onCancel = ()=>{
+    //     console.log('onCancel');
+    //     setIsButton(false)
+    // }
 
     let num = 0;
     return(
@@ -98,7 +69,22 @@ function Notice(props)
                 <div className="title">
                     <h3> Notice </h3>
                 </div>   
+                <div>
+                <Accordion alwaysOpen>
+                {
+                    noticeList.length!=0?
+                    noticeList.map((notice,i)=>{
+                        return <Accordion.Item eventKey={i}>
+                        <Accordion.Header>{notice.title}</Accordion.Header>
+                        <Accordion.Body bsPrefix='accordion-body'>
+                        {notice.text}
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    }) : null 
 
+                }
+                </Accordion>
+                </div>   
             </div>
         }
         </div> 
